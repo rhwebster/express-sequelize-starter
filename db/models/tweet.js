4230bd0f734e19fe7fc57ac732d23922356542e1
Ryan Webster <ryan.h.webster@gmail.com>
2020-11-25 11:10:37 -0800

    completing phase 1

---
+++ db/models/tweet.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tweet = sequelize.define('Tweet', {
+    message: DataTypes.STRING(280)
+  }, {});
+  Tweet.associate = function(models) {
+    // associations can be defined here
+  };
+  return Tweet;
+};
